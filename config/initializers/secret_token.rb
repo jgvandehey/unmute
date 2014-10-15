49012294eff8a6e8e222cd33dbb2d50a411adda2
Jeremy Vandehey <jgvandehey@gmail.com>
2014-10-15 14:29:46 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Unmute::Application.config.secret_token = 'fe8b19fe5fcfe386e4ddbbee68eec66c0a845e966a71aa6bfd8fa97a3e56a86c7c339ef6a4d51dc819d77818bed8789b029053efabdd957ab538cf1788608ed0'
