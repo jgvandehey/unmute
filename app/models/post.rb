3b24004f98eebe6028c32929c2909e3c3323ff4e
Jeremy Vandehey <jgvandehey@gmail.com>
2014-10-16 22:12:32 -0700

    add well to post/index

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,5 @@ class Post < ActiveRecord::Base
 	validates :title, presence: true
 	belongs_to :user
 	validates :user_id, presence: true
-	has_attached_file :image, :styles => { :medium => "300x300#", :thumb => "100x100#" }, :default_url => "https://s3-us-west-2.amazonaws.com/unmute/icon_400.png"  
+	has_attached_file :image, :styles => { :medium => "300x300#", :thumb => "100x100#" }, :default_url => "https://s3.amazonaws.com/unmute/icon_400.png"  
 end
