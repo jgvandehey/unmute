33f7265cee6ddddc1a9eda107f81e5ded850b921
Jeremy Vandehey <jgvandehey@gmail.com>
2014-10-16 17:33:01 -0700

    add paperclip and post styling

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,8 @@
 class Post < ActiveRecord::Base
-	attr_accessible :body, :title, :user_id, :publish
+	attr_accessible :body, :title, :user_id, :publish, :category, :image
 
 	validates :title, presence: true
 	belongs_to :user
 	validates :user_id, presence: true
+	has_attached_file :image, :styles => { :medium => "300x300#", :thumb => "100x100#" }, :default_url => "https://s3-us-west-2.amazonaws.com/unmute/icon_400.png"  
 end
